e35c9265d804e97e834b068b61de4ff9d1da71f9
Anthony Kinsey <anthony@slickage.com>
2016-06-06 16:42:24 -1000

    Added route and db methods for leaving trust feedback

---
+++ index.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var db = require(path.normalize(__dirname + '/db'));
+//var hooks = require(path.normalize(__dirname + '/hooks'));
+var routes = require(path.normalize(__dirname + '/routes'));
+//var permissions = require(path.normalize(__dirname + '/permissions'));
+
+module.exports =  {
+  name: 'userTrust',
+//  permissions: permissions,
+  routes: routes,
+//  hooks: hooks,
+  db: db
+};
---
+++ routes/addTrustFeedback.js
@@ -0,0 +1,40 @@
+var Joi = require('joi');
+
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup Trust
+  * @api {POST} /trust Add Trust Feedback
+  * @apiName AddTrustFeedback
+  * @apiPermission User
+  * @apiDescription Used to mark a user as watching a board.
+  *
+  * @apiError (Error 500) InternalServerError There was an issue adding feedback
+  */
+module.exports = {
+  method: 'POST',
+  path: '/api/trust',
+  config: {
+    auth: { strategy: 'jwt' },
+    validate: {
+      payload: {
+        user_id: Joi.string().required(),
+        risked_btc: Joi.number(),
+        scammer: Joi.boolean().required(),
+        reference: Joi.string().min(3).max(1024),
+        comments: Joi.string().min(3).max(1024)
+      }
+    }
+  },
+  handler: function(request, reply) {
+    var opts = {
+      userId: request.payload.user_id,
+      reporterId: request.auth.credentials.id,
+      riskedBtc: request.payload.risked_btc,
+      scammer: request.payload.scammer,
+      reference: request.payload.reference,
+      comments: request.payload.comments
+    };
+    var promise = request.db.userTrust.addTrustFeedback(opts);
+    return reply(promise);
+  }
+};
